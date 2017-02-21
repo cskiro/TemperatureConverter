dc8dfde6f0a9a93116f0d2a5887c2cedb12bd962
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-02-21 15:39:46 -0500

    Allowing user to convert temperature in either degrees Fahrenheit or degrees Celsius

--- src/temperatureconverter/TemperatureConverter.java
+++ src/temperatureconverter/TemperatureConverter.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 
 /**
  * Chapter 2 Exercise 6
- * 
+ *
  * Write a program that converts degrees Fahrenheit to degrees Celsius.
- * 
+ *
  * @author skiroc
  */
 public class TemperatureConverter {
@@ -21,22 +21,43 @@ public class TemperatureConverter {
      */
     public static void main(String[] args) {
         // Variables
-        double degreesCelsius, degreesFahrenheit; 
-        
+        double degreesCelsius, degreesFahrenheit;
+
         // Creating Scanner object
         Scanner kb = new Scanner(System.in);
-        
-        // Prompting user to enter a temperature in degrees Fahrenheit
-        System.out.println("Please enter a temperature in degrees Fahrenheit:");
-        degreesFahrenheit = kb.nextDouble();
-        
-        // Converting degrees Fahrenheit to degrees Celsius
-        degreesCelsius = (degreesFahrenheit - 32) * (0.5556);
-        
-        // Displaying converted temperature in degrees Celsius
-        System.out.println(degreesFahrenheit + " degrees Fahrenheit equals "
-        + degreesCelsius + " degrees Celsius.");
-        
+
+        // Prompting user to select whether they want to convert a temperature
+        // to degrees Fahrenheit or degrees Celsius
+        System.out.println("Press 'C' to convert your temperature to degrees Celsius"
+                + " and 'F' to convert your temperature to degrees Fahrenheit..");
+
+        if (kb.next().equalsIgnoreCase("C")) {
+            // Prompting user to enter a temperature in degrees Fahrenheit
+            System.out.println("Please enter a temperature in degrees Fahrenheit:");
+            degreesFahrenheit = kb.nextDouble();
+
+            // Converting degrees Fahrenheit to degrees Celsius
+            degreesCelsius = (degreesFahrenheit - 32) * (0.5556);
+
+            // Displaying converted temperature in degrees Celsius
+            System.out.println(degreesFahrenheit + " degrees Fahrenheit equals "
+                    + degreesCelsius + " degrees Celsius.");
+        } else if (kb.next().equalsIgnoreCase("F")) {
+            // Prompting user to enter a temperature in degrees Celcius
+            System.out.println("Please enter a temperature in degrees Celcius:");
+            degreesCelsius = kb.nextDouble();
+
+            degreesFahrenheit = 32 + (degreesCelsius * 9 / 5);
+
+            // Displaying converted temperature in degrees Celsius
+            System.out.println(degreesCelsius + " degrees Celsius equals "
+                    + degreesFahrenheit + " degrees Fahrenheit.");
+        } else {
+            System.out.println("Invalid input");
+            System.exit(0);
+            
+        } //end if-else
+
     } //end main 
-    
+
 } //end class
